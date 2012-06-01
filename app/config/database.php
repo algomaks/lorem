71bc1fecb39d56d1ad8632185e988f6aa079599d
Martin Minchev <martin.m.minchev@gmail.com>
2012-06-01 21:03:38 +0200

    second commit

---
+++ app/config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'master1',
+		'database' => 'lorem',
+		'encoding' => 'utf8'
+	);
+}
+?>
