ed0215b2c8a27f7a4bed12818ef09405138e6f4b
Christine Gerpheide <christine.ger@pheide.com>
2012-06-01 23:34:42 +0200

    A

--- app/controllers/main_controller.php
+++ app/controllers/main_controller.php
@@ -7,5 +7,41 @@ class MainController extends AppController {
 	function index() 
 	{
 		
+		
+		
+		//NEEDED VARS
+		  $people = array(
+		   		array(
+		   			'first_name' => 'Christine',
+		   			'last_name' => 'Gerpheide',
+		   			'gender' => 'F',
+		   			'nationality' => 'Mexico',
+		   			'languages' => 'English',
+		   			'interests' => 'Sailing, Climbing',
+		   			'coordinates' => '51.439136,5.475483',
+		   			'image' => 'http://images.paraorkut.com/img/clipart/images/s/sad_person-857.png',
+		   			'connect_link' => 'aoeu',
+		   		),
+		   		array(
+		   			'first_name' => 'Gerardo',
+		   			'last_name' => 'Ochoa',
+		   			'gender' => 'M',
+		   			'nationality' => 'Mexico',
+		   			'languages' => 'English',
+		   			'interests' => 'Sailing, Climbing',
+		   			'coordinates' => '51.441642,5.469722',
+		   			'image' => 'http://images.paraorkut.com/img/clipart/images/s/sad_person-857.png',
+		   			'connect_link' => 'aoeu',
+		   		),
+		   	);
+		   	
+		   	$this->set('people', $people);
+		
+	}
+	
+	function welcome()
+	{
+		
+		
 	}
 }
